4c16eb3e39f10de8294fc2fafb18b52828d56507
JiadanWang <jiadanwang@gmail.com>
2019-07-21 23:49:10 +0000

    slide new tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,20 +6,20 @@
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-$(document).ready(function() {
+$(document).ready(function () {
   $(".container0").hide();
-  const createTweetElement = function(tweet) {
+  const createTweetElement = function (tweet) {
     let $tweet = $('<article>').addClass('tweet');
     let $userinput = $('<div>').text(tweet.content.text).html();
-    
+
     $tweet.html(`
-              <div class="container1">
-              <div class="tweet-header">
+            <div class="container1">
+            <div class="tweet-header">
             <img class="avatar" src=${tweet.user.avatars} style="width: 35px">
             <h6 class="tweet-username">${tweet.user.name}</h6>
             <div class="tweet-handle">${tweet.user.handle}</div>
-          </div>
-          <h6 class="FirstT">${$userinput}
+            </div>
+            <h6 class="FirstT">${$userinput}
           </h6>
           <div class="footer">
           <div class="line"></div>
@@ -35,13 +35,13 @@ $(document).ready(function() {
     return $tweet;
   }
 
-  const renderTweets = function(tweets) {
-    tweets.forEach((tweet)=>{
-       const $tweet = createTweetElement(tweet);
+  const renderTweets = function (tweets) {
+    tweets.forEach((tweet) => {
+      const $tweet = createTweetElement(tweet);
       console.log($tweet)
       $('#tweets-container').prepend($tweet);
     })
-   }
+  }
 
 
   const $form = $('#new-tweet');
@@ -49,33 +49,34 @@ $(document).ready(function() {
     event.preventDefault()
   })
 
-  const loadTweets = function() {
-    
-        $.ajax(' /tweets', { method: 'GET' })
-        .then(renderTweets);
-}
+  const loadTweets = function () {
+
+    $.ajax(' /tweets', { method: 'GET' })
+      .then(renderTweets);
+  }
 
   loadTweets();
 
-  const postTweets = function() {
-    
+  const postTweets = function () {
+
     let a = $(".tweetForm").val().length;
     if (a > 140 || a === 0) {
       $(".container0").show();
       $('.container0').fadeOut(3000);
     } else {
-    $.post('/tweets', $('#new-tweet').serialize(), (tweet) => {
-      $('#tweets-container').empty();
-      $(".container0").hide();
-      loadTweets();
-    }) } 
+      $.post('/tweets', $('#new-tweet').serialize(), (tweet) => {
+        $('#tweets-container').empty();
+        $(".container0").hide();
+        loadTweets();
+      })
+    }
 
   }
   $('.tweetBotton').click(postTweets)
-  
+
   $('.new-tweet').hide();
   $('.double-arrow').click(() => {
-    $('.new-tweet').toggle()
+    $('.new-tweet').slideToggle();
   })
 });
 
